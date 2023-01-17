c3daeb5f0cf35ff4e1385b7af7e6392c85bce309
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-17 13:25:09 +0100

    First commit

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    [SerializeField] private float _GizmosRadius;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(transform.position, _GizmosRadius);
+    }
+}
