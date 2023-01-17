47184b11ff1c3214ab0eefb0fc31c18fe77b4238
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-17 19:35:34 +0100

    Fix Coroutine behaviour

--- Assets/Scripts/CubeBehaviour.cs
+++ Assets/Scripts/CubeBehaviour.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class CubeBehaviour : MonoBehaviour
 {
-    private int currentWaypointIndex = 2;
-
-    [SerializeField] private float _speed = 5f;
     [SerializeField] private GameObject _prefab;
     [SerializeField] private List<Transform> _waypoints = new List<Transform>();
 
@@ -35,12 +32,31 @@ public class CubeBehaviour : MonoBehaviour
 
     private IEnumerator ObjMovementTowardsWaypoint()
     {
-        foreach (Transform _waypoint in _waypoints)
+        if (gameObject.name == "Point B")
         {
-            GameObject objSpawned = Instantiate(_prefab, _waypoint.position, Quaternion.identity);
+            GameObject objSpawned = Instantiate(_prefab, gameObject.transform.position, Quaternion.identity);
+            objSpawned.transform.position = _waypoints[0].position;
 
-            objSpawned.transform.position = Vector3.MoveTowards(objSpawned.transform.position, _waypoint.position, _speed * Time.deltaTime);
-            yield return new WaitForSeconds(2);
+            for (int i = 0; i < _waypoints.Count - 1; i++)
+            {
+                yield return LerpCoroutine(objSpawned, _waypoints[i].position, _waypoints[i + 1].position);
+            }
+
+            Destroy(objSpawned);
         }
+    }
+
+    private IEnumerator LerpCoroutine(GameObject objToLerp, Vector3 start, Vector3 end)
+    {
+        float t = 0f;
+
+        while (t <= 1f)
+        {
+            objToLerp.transform.position = Vector3.Lerp(start, end, t);
+            t += Time.deltaTime / 2;
+            yield return null; // We have to wait for the next frame
+        }
+
+        objToLerp.transform.position = end;
     }
 }
