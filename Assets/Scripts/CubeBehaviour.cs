244e4301b2f33420fe84f1087213e82bd2cdd155
hendrickl <65900996+hendrickl@users.noreply.github.com>
2023-01-17 16:19:45 +0100

    Implement a coroutine to add movement to spawned obj towards waypoint

--- Assets/Scripts/CubeBehaviour.cs
+++ Assets/Scripts/CubeBehaviour.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class CubeBehaviour : MonoBehaviour
 {
+    private int currentWaypointIndex = 2;
+
+    [SerializeField] private float _speed = 5f;
     [SerializeField] private GameObject _prefab;
     [SerializeField] private List<Transform> _waypoints = new List<Transform>();
 
-    private void Update()
+    private void OnMouseDown()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (gameObject.name == "Point A")
         {
             StartCoroutine(SpawnObjectAtWaypointPosition());
+        }
+
+        if (gameObject.name == "Point B")
+        {
+            StartCoroutine(ObjMovementTowardsWaypoint());
         }
     }
 
@@ -22,6 +30,17 @@ public class CubeBehaviour : MonoBehaviour
             GameObject objSpawned = Instantiate(_prefab, _waypoint.position, Quaternion.identity);
             yield return new WaitForSeconds(2);
             Destroy(objSpawned);
+        }
+    }
+
+    private IEnumerator ObjMovementTowardsWaypoint()
+    {
+        foreach (Transform _waypoint in _waypoints)
+        {
+            GameObject objSpawned = Instantiate(_prefab, _waypoint.position, Quaternion.identity);
+
+            objSpawned.transform.position = Vector3.MoveTowards(objSpawned.transform.position, _waypoint.position, _speed * Time.deltaTime);
+            yield return new WaitForSeconds(2);
         }
     }
 }
